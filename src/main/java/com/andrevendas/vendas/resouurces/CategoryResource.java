24674df65c9cdf69435cbc6b6209d752d868fa9f
andre <falconi879@gmail.com>
2022-10-21 09:42:38 -0300

    Endpoint buscando no banco de dados 'Todas Categorias e buscando Categorias por ID'

--- src/main/java/com/andrevendas/vendas/resouurces/CategoryResource.java
+++ src/main/java/com/andrevendas/vendas/resouurces/CategoryResource.java
@@ -1,23 +1,33 @@
 package com.andrevendas.vendas.resouurces;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andrevendas.vendas.entities.Category;
+import com.andrevendas.vendas.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categorias") 
 public class CategoryResource {
 	
+	@Autowired
+	CategoryService servico;
+	
 	@GetMapping
-	public ResponseEntity<Category> findAll(){
-		Category cat1 = new Category(1L,"Informatica");
-		return ResponseEntity.ok().body(cat1);
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> lista = servico.findAll();
+		return ResponseEntity.ok().body(lista);
 	}
 	
-	
-	
-
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		Category objeto = servico.findById(id);
+		return ResponseEntity.ok().body(objeto);
+	}
 }
